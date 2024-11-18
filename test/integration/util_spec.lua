d72af735cf74eec4aaa923ee599b81f9ea9ba5ac
Chris Kedzie <ckedzie@scaledcognition.com>
2024-11-18 10:08:12 -0500

    Use better buffer text insertion method. Clean up utils. Add tests.

--- test/integration/util_spec.lua
+++ test/integration/util_spec.lua
@@ -1,3 +1,5 @@
+local assert = require("busted").assert
+
 local jobopts = { rpc = true, width = 80, height = 24 }
 
 describe("Util", function()
@@ -91,4 +93,182 @@ describe("Util", function()
 		assert.is.equal("foo\nbar", overwritten_buffer_text)
 
 	end)
+
+    it(".buffer.insert_text (insert single line text at beginning of line)", function()
+		local buffer_id = vim.rpcrequest(nvim, "nvim_exec_lua", 'return require("loom.util").buffer.new()', {})
+
+		vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.set_text(%d, "foo\\nalphaomega\\nbar")', buffer_id),
+			{}
+		)
+
+		local end_position = vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.insert_text(%d, 1, 0, "*")', buffer_id),
+			{}
+		)
+
+		local buffer_text = vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.get_text(%d)', buffer_id),
+			{}
+		)
+
+        assert.is.True(end_position ~= nil)
+        assert.is.True(end_position.line ~= nil)
+        assert.is.True(end_position.column ~= nil)
+        assert.is.equal(1, end_position.line)
+        assert.is.equal(1, end_position.column)
+		assert.is.equal("foo\n*alphaomega\nbar", buffer_text)
+
+	end)
+
+    it(".buffer.insert_text (insert single line text in middle of line)", function()
+		local buffer_id = vim.rpcrequest(nvim, "nvim_exec_lua", 'return require("loom.util").buffer.new()', {})
+
+		vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.set_text(%d, "foo\\nalphaomega\\nbar")', buffer_id),
+			{}
+		)
+
+		local end_position = vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.insert_text(%d, 1, 5, " middle ")', buffer_id),
+			{}
+		)
+
+        print("\n")
+        print("line   >>" .. end_position.replacement_line .. "<<")
+        print("prefix >>" .. end_position.prefix .. "<<")
+        print("suffix >>" .. end_position.suffix .. "<<")
+
+		local buffer_text = vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.get_text(%d)', buffer_id),
+			{}
+		)
+        print("buffer text >>" .. buffer_text .. "<<")
+        print("\n")
+        assert.is.True(end_position ~= nil)
+        assert.is.True(end_position.line ~= nil)
+        assert.is.True(end_position.column ~= nil)
+        assert.is.equal(1, end_position.line)
+        assert.is.equal(13, end_position.column)
+		assert.is.equal("foo\nalpha middle omega\nbar", buffer_text)
+
+	end)
+
+    it(".buffer.insert_text (insert single line text at end of line)", function()
+		local buffer_id = vim.rpcrequest(nvim, "nvim_exec_lua", 'return require("loom.util").buffer.new()', {})
+
+		vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.set_text(%d, "foo\\nalphaomega\\nbar")', buffer_id),
+			{}
+		)
+
+		local end_position = vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.insert_text(%d, 1, 10, "*")', buffer_id),
+			{}
+		)
+
+		local buffer_text = vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.get_text(%d)', buffer_id),
+			{}
+		)
+
+        assert.is.True(end_position ~= nil)
+        assert.is.True(end_position.line ~= nil)
+        assert.is.True(end_position.column ~= nil)
+        assert.is.equal(1, end_position.line)
+        assert.is.equal(11, end_position.column)
+		assert.is.equal("foo\nalphaomega*\nbar", buffer_text)
+
+	end)
+
+    it(".buffer.insert_text (insert 0 length line)", function()
+		local buffer_id = vim.rpcrequest(nvim, "nvim_exec_lua", 'return require("loom.util").buffer.new()', {})
+
+		vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.set_text(%d, "foo\\nalphaomega\\nbar")', buffer_id),
+			{}
+		)
+
+		local end_position = vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.insert_text(%d, 1, 5, "")', buffer_id),
+			{}
+		)
+
+		local buffer_text = vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.get_text(%d)', buffer_id),
+			{}
+		)
+        assert.is.True(end_position ~= nil)
+        assert.is.True(end_position.line ~= nil)
+        assert.is.True(end_position.column ~= nil)
+        assert.is.equal(1, end_position.line)
+        assert.is.equal(5, end_position.column)
+		assert.is.equal("foo\nalphaomega\nbar", buffer_text)
+
+	end)
+
+    it(".buffer.insert_text (insert multi line text in middle of line)", function()
+		local buffer_id = vim.rpcrequest(nvim, "nvim_exec_lua", 'return require("loom.util").buffer.new()', {})
+
+		vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.set_text(%d, "foo\\nalphaomega\\nbar")', buffer_id),
+			{}
+		)
+
+		local end_position = vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.insert_text(%d, 1, 5, " line1\\nline2 ")', buffer_id),
+			{}
+		)
+
+        -- TODO(kedz): Remove extra return arguments.
+        print("\n")
+        print("line   >>" .. end_position.replacement_line .. "<<")
+        print("prefix >>" .. end_position.prefix .. "<<")
+        print("suffix >>" .. end_position.suffix .. "<<")
+
+		local buffer_text = vim.rpcrequest(
+			nvim,
+			"nvim_exec_lua",
+			string.format('return require("loom.util").buffer.get_text(%d)', buffer_id),
+			{}
+		)
+        print("buffer text >>" .. buffer_text .. "<<")
+        print("\n")
+        assert.is.True(end_position ~= nil)
+        assert.is.True(end_position.line ~= nil)
+        assert.is.True(end_position.column ~= nil)
+        assert.is.equal(2, end_position.line)
+        assert.is.equal(6, end_position.column)
+		assert.is.equal("foo\nalpha line1\nline2 omega\nbar", buffer_text)
+
+	end)
+
 end)
